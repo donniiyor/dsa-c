bb113419e7c67abbfb6aacc6fd682cd69d262d31
Doniyor <doniyor.otamurodov@gmail.com>
2026-07-18 20:15:21 +0500

    feat: update stack types

--- src/stack.h
+++ src/stack.h
@@ -3,9 +3,12 @@
 #include <stddef.h>
 
 typedef struct {
-    size_t count;
-    size_t capacity;
-    void **data;
+  size_t count;
+  size_t capacity;
+  void **data;
 } Stack;
 
-Stack* stack_new(size_t capacity);
+void stack_free(Stack *stack);
+void *stack_pop(Stack *stack);
+void stack_push(Stack *stack, void *obj);
+Stack *stack_new(size_t capacity);
