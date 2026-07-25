d9f2f0bf6837dd3558169bd833ae791fe1393c7f
Doniyor <doniyor.otamurodov@gmail.com>
2026-07-25 23:02:03 +0500

    feat: init hashtable

---
+++ include/hashtable.h
@@ -0,0 +1,22 @@
+#include <stddef.h>
+
+struct pair {
+    char *key;
+    void *value;
+};
+
+struct hashtable {
+    struct vector *buckets;
+    size_t size;
+    size_t buckets_count;
+};
+
+struct hashtable *hashtable_create(size_t capacity, size_t buckets_count);
+void hashtable_destro(struct hashtable *ht);
+
+bool hashtable_put(struct hashtable *ht, const char *key, const void *value);
+bool hashtable_get(const struct hashtable *ht, const char *key, void *value);
+
+bool hashtable_remove(struct hashtable *ht, const char *key);
+
+size_t hashtable_size(struct hashtable *ht);
---
+++ src/hashtable.c
@@ -0,0 +1,73 @@
+#include "hashtable.h"
+#include "vector.h"
+
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct hashtable *hashtable_create(size_t capacity, size_t buckets_count) {
+    struct hashtable *ht = malloc(sizeof(struct hashtable));
+    if (ht == NULL) return NULL;
+
+    ht->buckets = calloc(buckets_count, sizeof(struct vector));
+    if (ht->buckets == NULL) {
+        free(ht);
+        return NULL;
+    }
+
+    ht->buckets_count = buckets_count;
+    ht->size = 0;
+
+    return ht;
+}
+
+void hashtable_destro(struct hashtable *ht) {
+    assert(ht != NULL);
+
+    for (uint16_t i = 0; i < ht->buckets_count; i++) {
+        vector_destroy(&ht->buckets[i]);
+    }
+
+    free(ht);
+}
+
+static uint64_t hash(const char *s) {
+    uint64_t h = 14695981039346656037ULL;
+
+    while (*s) {
+        h ^= (unsigned char)*s++;
+        h *= 1099511628211ULL;
+    }
+
+    return h;
+}
+
+bool hashtable_put(struct hashtable *ht, const char *key, const void *value) {
+    assert(ht != NULL);
+
+    size_t index = hash(key) % ht->buckets_count;
+    struct vector bucket = ht->buckets[index];
+
+    int item_index = -1;
+
+    for (int i = 0; i < bucket.size; i++) {
+        struct pair p;
+        vector_get(&bucket, i, (void *)&p);
+
+        if (strcmp(p.key, key) == 0) {
+            item_index = i;
+            break;
+        }
+    }
+
+    struct pair p;
+    memcpy(p.key, key, sizeof(char));
+    memcpy(p.value, value, sizeof(void *));
+
+    if (item_index == -1) vector_push(&bucket, &p);
+    else vector_set(&bucket, item_index, &p);
+
+    return true;
+}
